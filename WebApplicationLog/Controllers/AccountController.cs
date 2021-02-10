b1d0d6962331e07fbd763918e0d7a84593745f20
Sumit <mitrasumit14@gmail.com>
2021-02-10 21:13:28 +0530

    Initial commit

---
+++ WebApplicationLog/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationLog.Models;
+using System.Web.Security;
+
+namespace WebApplicationLog.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x=>x.UserName==model.UserName && x.Password==model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName,false);
+                    return RedirectToAction("Index","Employees");
+                }
+                ModelState.AddModelError("","Invalid username and password");
+                return View();
+            }
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ WebApplicationLog/Models/Membership.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationLog.Models
+{
+    public class Membership
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
